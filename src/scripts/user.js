bb2ed39061d397c4d3d5d31ae02b6c9646277144
kazemi-hub <58529598+kazemi-hub@users.noreply.github.com>
2019-12-30 23:09:44 +0330

    add planet section

--- src/scripts/user.js
+++ src/scripts/user.js
@@ -11,8 +11,8 @@ $(function() {
   $(".sidebar").show();
   var tll= new TimelineLite();
   tll
-    .from(icon0,0.5,{y:200,autoAlpha:0,ease:Power1.bonus, opacity:0},2)
-    .from(icon1,0.2,{y:250,autoAlpha:0,ease:Power2.easeOut, opacity:0})
+    .from(icon1,0.5,{y:200,autoAlpha:0,ease:Power1.bonus, opacity:0},2)
+    .from(icon0,0.2,{y:250,autoAlpha:0,ease:Power2.easeOut, opacity:0})
     .from(icon2,0.2,{y:300,autoAlpha:0,ease:Power2.easeOut, opacity:0})
     .from(icon3,0.2,{y:350,autoAlpha:0,ease:Power2.easeOut, opacity:0})
     .from(icon4,0.2,{y:400,autoAlpha:0,ease:Power2.easeOut, opacity:0})
