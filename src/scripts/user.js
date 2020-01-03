bb0a28c13e8ec095d3b046e9f0e8b9cde03c7678
kazemi-hub <58529598+kazemi-hub@users.noreply.github.com>
2020-01-04 00:47:39 +0330

    add dark-mode and scroll-view

--- src/scripts/user.js
+++ src/scripts/user.js
@@ -1,5 +1,5 @@
 $(function() {
-
+// navigator button Effects
   var icon0=$("#icon1");
   var icon1=$(".icon2");
   var icon2=$(".icon3");
@@ -20,7 +20,7 @@ $(function() {
     .from(icon6,0.2,{y:500,autoAlpha:0,ease:Power2.easeOut, opacity:0})
     .from(icon7,0.5,{y:550,autoAlpha:0,ease:Power2.easeOut, opacity:0})
     
-
+  //welcome Slide
     var slideIndex = 0;
     showSlides();
     
@@ -37,7 +37,18 @@ $(function() {
       slides[slideIndex - 1].style.display = "block";
       setTimeout(showSlides, 4000);
     }
-    
+
+    //scroll to top 
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 50 ) {
+          $('.scrolltop:hidden').stop(true, true).fadeIn();
+      } else {
+          $('.scrolltop').stop(true, true).fadeOut(5000);
+      }
+  });
+  $(function(){$(".scroll").click(function(){$("html,body").animate({scrollTop:$("#header").offset().top},"1000");return false})})
+
+  //Planet Slide
     $(".slide").hiSlide();
 
     console.log("it worked..")
